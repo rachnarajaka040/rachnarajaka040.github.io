b06feb243450b3fe5608b5bed31259e45b967323
rachna rajak <113829584+rachnarajaka040@users.noreply.github.com>
2024-03-08 22:46:08 +0530

    up

--- src/components/GitHubStats.jsx
+++ src/components/GitHubStats.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const GitHubStats = () => {
     return (
-        <Container maxW={['full','full','60%','full','full']}  gap='10px' m='auto' marginTop='20'>
+        <Container maxW={['full','full','60%','full','full']}  gap='10px' m='auto'marginTop='20'>
             <Center gap='10px'display={['block','block','block','flex','flex']}>
 
         <Image src='https://camo.githubusercontent.com/c847100912e35749fbb4035001b49ac443bb0e2fe0ae1a6a6c0dec755b78dd18/68747470733a2f2f6769746875622d726561646d652d73747265616b2d73746174732e6865726f6b756170702e636f6d2f3f757365723d726163686e6172616a616b61303430267468656d653d6461726b'/>
