be11db0a926a0c1d4ba37ce8b2ce5a30a0418e67
rachnarajaka040 <rachnarajak040@gmail.com>
2024-03-18 10:00:32 +0530

    updated images

--- src/components/MyImageCard.jsx
+++ src/components/MyImageCard.jsx
@@ -1,43 +1,35 @@
-import {  Container } from '@chakra-ui/react';
-import React from 'react';
+import { Container } from "@chakra-ui/react";
+import React from "react";
 import rachna from "./rachna.png";
 
 const MyImageCard = () => {
-    return (
-       
-        <Container transform= 'rotate(45deg)'
-    borderRadius= {['2em','3em','']}
-    height= {['200px','300px','300px']}
-    width= {['200px','300px','300px']}
-    overflow='hidden'
-    bg="#A75D5D"
-    color="white"
-    padding='0px'
-    mt={['10em','10em','-0em','-0em','-0em']}
-    mr={['10em','15em','15em','20em','5em']}
-    _hover={{boxShadow:'5px 5px 20px #79d392'}}    
+  return (
+    <Container
+      transform="rotate(45deg)"
+      borderRadius={["2em", "3em", ""]}
+      height={["200px", "300px", "300px"]}
+      width={["200px", "300px", "300px"]}
+      overflow="hidden"
+      bg="#A75D5D"
+      color="white"
+      padding="0px"
+      mt={["10em", "10em", "-0em", "-0em", "-0em"]}
+      mr={["10em", "15em", "15em", "20em", "5em"]}
+      _hover={{ boxShadow: "5px 5px 20px #79d392" }}
     >
-        <Container transform= 'rotate(-45deg)'
-         height= {['250px','380px','380px']}
-         width= {['250px','380px','380px']}
-         marginBottom='20px'
-         bgPos='center'
-         bgRepeat='no-repeat'
-         bgSize='contain'
-         bgImage={rachna}
-        
-         m='0px'
-         ></Container>
-       
-        
-    
-      </Container>
-    );
+      <Container
+        transform="rotate(-45deg)"
+        height={["250px", "380px", "380px"]}
+        width={["250px", "380px", "380px"]}
+        marginBottom="20px"
+        bgPos="center"
+        bgRepeat="no-repeat"
+        bgSize="contain"
+        bgImage={rachna}
+        m="0px"
+      ></Container>
+    </Container>
+  );
 };
 
 export default MyImageCard;
-
-
-  
- 
-  
