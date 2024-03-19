de92a9494b62dbe74efefb8f55b8f67ceeef3a04
rachnarajaka040 <rachnarajak040@gmail.com>
2024-03-19 10:27:25 +0530

    responsive

--- src/components/SummaryAboutme.jsx
+++ src/components/SummaryAboutme.jsx
@@ -1,4 +1,12 @@
-import { Container, Divider, Box,Icon, Text, Flex,Link } from "@chakra-ui/react";
+import {
+  Container,
+  Divider,
+  Box,
+  Icon,
+  Text,
+  Flex,
+  Link,
+} from "@chakra-ui/react";
 import React from "react";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { IoLocation } from "react-icons/io5";
@@ -7,40 +15,46 @@ import { MdPhone, MdEmail } from "react-icons/md";
 const SummaryAboutme = () => {
   return (
     <Container
-    id='About'
+      id="About"
       transform="rotate(45deg)"
       borderRadius="2em"
-      height={['280px',"450px"]}
-      width={['280px',"450px"]}
+      height={["280px", "450px"]}
+      width={["280px", "450px"]}
       bg="#A75D5D"
       color="white"
       p="0px"
-      mt={['2em','0em','2em','0em','0em']}
-      mr={['1.5em','5em','10em','20em','25em']}
-      _hover={{boxShadow:'5px 5px 20px #79d392'}}
+      mt={["2em", "0em", "2em", "0em", "0em"]}
+      mr={["1.5em", "5em", "10em", "20em", "25em"]}
+      _hover={{ boxShadow: "5px 5px 20px #79d392" }}
     >
       <Container
         transform="rotate(-45deg)"
-        height={['280px',"450px"]}
-        width={['280px',"450px"]}
-        py={['.8em','2.5em']}
+        height={["280px", "450px"]}
+        width={["280px", "450px"]}
+        py={[".8em", "3.5em"]}
       >
-        
-        <Text fontSize={['1em',"1.5em"]} textAlign="center" fontWeight="bold">
+        <Text fontSize={["1em", "1.5em"]} textAlign="center" fontWeight="bold">
           About Me
         </Text>
-        <Divider w={['100px','200px']} m='auto' my={['5px','10px']} borderColor="brand_secondary" />
+        <Divider
+          w={["100px", "200px"]}
+          m="auto"
+          my={["5px", "10px"]}
+          borderColor="brand_secondary"
+        />
 
-        <Text fontSize={['xs',"md"]} margin="auto">
-        Hello! I'm Rachna Rajak , a passionate Full Stack Web Developer with a Bachelor's degree in Computer Science . Utilizing cutting-edge technologies to build amazing products and focusing on solving problems. With 9 months of hands-on experience as a Frontend Developer, I've actively contributed to projects, honing my skills in Html, Css , JavaScript, React.js , Next.js etc. Look forward to bringing innovation and expertise to your team or project.
+        <Text fontSize={["xs", "md"]} margin="auto" textAlign="center">
+          Hello! I'm Rachna Rajak, a passionate Full Stack Web Developer with a
+          Bachelor's degree in Computer Science. Utilizing cutting-edge
+          technologies to build amazing products and focusing on solving
+          problems. With 9 months of hands-on experience as a Frontend
+          Developer, I've actively contributed to projects, honing my skills in
+          Html, Css, JavaScript, React.js, Next.js, etc. Look forward to
+          bringing innovation and expertise to your team or project.
         </Text>
-       
-       
       </Container>
     </Container>
   );
 };
-
-
 
 export default SummaryAboutme;
